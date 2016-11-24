d60f6059bf7b291d540b0c05e047f54b6f00357d
汤丰源 <1315803594@qq.com>
2016-11-24 17:06:29 +0800

    G 调用返回信息

---
+++ common/map.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tangtang on 2016/11/24.
+ */
+module.exports = {
+  'type': {
+    'A': ''
+  }
+};
--- common/resMsg.js
+++ common/resMsg.js
@@ -1,3 +1,19 @@
 /**
  * Created by tangtang on 2016/11/24.
  */
+global.G = {};
+G.resSuccessMsg = function (data={},options) {
+  return Object.assign({
+    res: 'SUCCESS',
+    data:data
+  },options)
+};
+G.resErrorMsg = function (code=-1,message='系统错误') {
+  return {
+    res:'FAILED',
+    error: {
+      code:code,
+      message:message
+    }
+  };
+}
